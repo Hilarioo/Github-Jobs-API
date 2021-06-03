f1ebfb5b332ec973e69ba43e2fd0fbd6e968d27d
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2021-06-03 08:31:50 -0700

    fixed vulnerabilities

--- src/Api/useFetchJobs.js
+++ src/Api/useFetchJobs.js
@@ -7,6 +7,7 @@ const ACTIONS = {
   ERROR: "error",
   NEXT_PAGE: "next-page",
 };
+
 // might need to navigate to -> https://cors-anywhere.herokuapp.com/corsdemo and click button to unlock
 const FAKE_PROXY = "https://cors-anywhere.herokuapp.com/";
 const BASE_URL = FAKE_PROXY + "https://jobs.github.com/positions.json";
@@ -35,23 +36,22 @@ const useFetchJobs = (params, page) => {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
   useEffect(() => {
-    const cancelToken = axios.CancelToken.source();
-    const cancelToken2 = axios.CancelToken.source();
+    const cancelToken1 = axios.CancelToken.source();
     dispatch({ type: ACTIONS.MAKE_REQUEST });
-
     axios
       .get(BASE_URL, {
-        cancelToken: cancelToken.token,
+        cancelToken: cancelToken1.token,
         params: { markdown: true, page: page, ...params },
       })
       .then((res) => {
         dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
       })
-      .catch((err) => {
-        if (axios.isCancel(err)) return;
-        dispatch({ type: ACTIONS.ERROR, payload: { error: err } });
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
       });
 
+    const cancelToken2 = axios.CancelToken.source();
     axios
       .get(BASE_URL, {
         cancelToken: cancelToken2.token,
@@ -63,13 +63,13 @@ const useFetchJobs = (params, page) => {
           payload: { hasNextPage: res.data.length !== 0 },
         });
       })
-      .catch((err) => {
-        if (axios.isCancel(err)) return;
-        dispatch({ type: ACTIONS.ERROR, payload: { error: err } });
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
       });
 
     return () => {
-      cancelToken.cancel();
+      cancelToken1.cancel();
       cancelToken2.cancel();
     };
   }, [params, page]);
